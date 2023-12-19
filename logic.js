ef903a16565c1b6baed021dc61ddf553bd8d924a
aryansnagar <imaryannagar@gmail.com>
2023-12-19 18:05:17 +0530

    redesigned

--- logic.js
+++ logic.js
@@ -1,3 +1,10 @@
 function email() {
      window.location.href = 'mailto:aryannagarco@gmail.com';
 }
+
+function grace() {
+     window.location.href = 'grace.html';
+}
+function turtle() {
+     window.location.href = 'https://aryansnagar.github.io/college-mini-project';
+}
